32b5f699d50ff667b6910bbcacd36d1315f1b4cf
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2012-06-19 12:56:14 +0100

    Started fixing the wrong use of the server as the namespace for vocabularies. To be continued...

---
+++ iserve-commons-vocabulary/src/main/java/uk/ac/open/kmi/iserve/commons/vocabulary/ISERVE.java
@@ -0,0 +1,28 @@
+/*
+   Copyright ${year}  Knowledge Media Institute - The Open University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package uk.ac.open.kmi.iserve.commons.vocabulary;
+
+import org.ontoware.rdf2go.model.node.URI;
+import org.ontoware.rdf2go.model.node.impl.URIImpl;
+
+public class ISERVE {
+
+	public static final String ISERVE_NS_URI = "http://iserve.kmi.open.ac.uk/ns/iserve#";
+	public static final String ISERVE_NS_PREFIX = "iserve";
+
+//	public static final String HAS_SHA1 = ISERVE_NS_URI + "hasSha1";
+
+}
