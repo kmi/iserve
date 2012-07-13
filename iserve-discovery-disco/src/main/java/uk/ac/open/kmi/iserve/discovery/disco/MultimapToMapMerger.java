2ffbf9644c40548abe04b0abf6d8d44198a66027
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2012-07-13 11:30:09 +0100

    New merger for Multimaps -> Maps so that we can treat homogeneously inner match results

---
+++ iserve-discovery-disco/src/main/java/uk/ac/open/kmi/iserve/discovery/disco/MultimapToMapMerger.java
@@ -0,0 +1,70 @@
+/*
+   Copyright 2012  Knowledge Media Institute - The Open University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package uk.ac.open.kmi.iserve.discovery.disco;
+
+import java.net.URL;
+import java.util.Map;
+
+import uk.ac.open.kmi.iserve.discovery.api.MatchResult;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+
+/**
+ * Functions for merging a Multimap of Match Results into a Map of Match Results
+ * 
+ * @author Carlos Pedrinaci (Knowledge Media Institute - The Open University)
+ */
+public enum MultimapToMapMerger implements Function<Multimap<URL, MatchResult>, Map<URL, MatchResult>> {
+	
+	/**
+	 * Perform the UNION of the results.
+	 * The resulting type of the Composite results is determined by the best match
+	 * {@see MatchResultsMerger}
+	 * 
+	 * @author Carlos Pedrinaci (Knowledge Media Institute - The Open University)
+	 */
+	UNION {
+		
+		@Override
+		public Map<URL, MatchResult> apply(Multimap<URL, MatchResult> input) {
+			return ImmutableMap.copyOf(Maps.transformValues(input.asMap(), 
+					MatchResultsMerger.UNION));
+		}
+		
+	},
+	
+	/**
+	 * Perform the INTERSECTION of the results.
+	 * The resulting type of the Composite results is determined by the worst match
+	 * except for the FAIL cases where we may have Partial Matches
+	 * {@see MatchResultsMerger}
+	 * 
+	 * @author Carlos Pedrinaci (Knowledge Media Institute - The Open University)
+	 */
+	INTERSECTION {
+
+		@Override
+		public Map<URL, MatchResult> apply(Multimap<URL, MatchResult> input) {
+			return ImmutableMap.copyOf(Maps.transformValues(input.asMap(), 
+					MatchResultsMerger.INTERSECTION));
+		}
+
+	}
+
+}
