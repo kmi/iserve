baf15e9f5f62ea8bf6675e3c976161e08bf071a5
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-04-30 19:07:34 +0100

    Added new vocabulary files for now. We may be changing to use schemaGen for an automated solution

---
+++ iserve-commons-vocabulary/src/main/java/uk/ac/open/kmi/iserve/commons/vocabulary/WSMO_LITE.java
@@ -0,0 +1,31 @@
+/*
+   Copyright ${year}  Knowledge Media Institute - The Open University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package uk.ac.open.kmi.iserve.commons.vocabulary;
+
+public class WSMO_LITE {
+
+	// Namespaces
+	public static final String NS = "http://www.wsmo.org/ns/wsmo-lite#";
+	public static final String NS_PREFIX = "wl";
+	
+	public static final String ONTOLOGY = NS + "Ontology";
+	public static final String FUNCTIONAL_CLASSIFICATION_ROOT = NS + "FunctionalClassificationRoot";
+	public static final String NONFUNCTIONAL_PARAMETER = NS + "NonfunctionalParameter";
+	public static final String CONDITION = NS + "Condition";
+	public static final String EFFECT = NS + "Effect";
+	
+	public static final String USES_ONTOLOGY = NS + "usesOntology";
+}
