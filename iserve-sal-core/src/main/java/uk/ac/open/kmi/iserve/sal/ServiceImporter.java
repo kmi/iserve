43be9b5a15de97826f2e978efe61e3cbcbf006c6
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-06-04 17:30:14 +0100

    Updated method name and overloaded it to also support files directly

--- iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/ServiceImporter.java
+++ iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/ServiceImporter.java
@@ -1,41 +1,50 @@
 /*
-   Copyright ${year}  Knowledge Media Institute - The Open University
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ * Copyright (c) 2013. Knowledge Media Institute - The Open University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.open.kmi.iserve.sal;
 
-import java.io.InputStream;
-
+import uk.ac.open.kmi.iserve.commons.model.Service;
 import uk.ac.open.kmi.iserve.sal.exception.ImporterException;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * Abstract class to be extended by Service Importers
+ * // TODO: Rename and move to other package
  * 
  * @author Dong Liu (Knowledge Media Institute - The Open University)
  */
 public interface ServiceImporter {
 
 	/**
-	 * Transform a service description, e.g. SAWSDL, OWL-S, hRESTS, etc., into
-	 * Minimal Service Model (MSM).
-	 * 
-	 * @param orginalDescription
-	 *        The semantic Web service description
-	 * @return An input stream of the RDF describing the service and conforming to MSM model
-	 */
-	public abstract InputStream transformStream(InputStream orginalDescription) throws ImporterException;
+     * Transform a stream with service description(s), e.g. SAWSDL, OWL-S, hRESTS, etc., into
+     * Minimal Service Model (MSM), Service instances.
+     *
+     * @param orginalDescription The semantic Web service description(s)
+     * @return A List with the services transformed conforming to MSM model
+     */
+    public abstract List<Service> transform(InputStream orginalDescription) throws ImporterException;
 
-
+    /**
+     * Transform a file into Minimal Service Model (MSM).
+     *
+     * @param orginalFile The file containing the semantic Web service description(s)
+     * @return A List with the services transformed conforming to MSM model
+     */
+    public abstract List<Service> transform(File orginalFile) throws ImporterException;
 
 }
