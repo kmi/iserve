c0a37f94395e62f55915764ae72b630cb5d7df50
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-06-07 10:07:21 +0100

    Rearranged test to obtain resources in a more configuration independent manner

--- iserve-commons-io/src/test/java/uk/ac/open/kmi/iserve/commons/io/ServiceReaderImplTest.java
+++ iserve-commons-io/src/test/java/uk/ac/open/kmi/iserve/commons/io/ServiceReaderImplTest.java
@@ -22,12 +22,12 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import uk.ac.open.kmi.iserve.commons.model.Service;
-import uk.ac.open.kmi.iserve.commons.model.util.Vocabularies;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,11 +41,11 @@ import java.util.List;
  */
 public class ServiceReaderImplTest {
 
-    private static final String TEST_RESOURCES_PATH = "/../test/resources/";
-    private static final String OWLS_TC3_SERVICES = "OWLS-TC3-MSM";
+    private static final String OWLS_TC3_SERVICES = "/OWLS-TC3-MSM";
     private static final Syntax SYNTAX = Syntax.TTL;
 
-    private List<String> testFolders;
+    //    private List<String> testFolders;
+    private List<URI> testFolders;
     private FilenameFilter ttlFilter;
     private ServiceReader reader;
     private ServiceWriter writer;
@@ -55,15 +55,10 @@ public class ServiceReaderImplTest {
 
         reader = new ServiceReaderImpl();
         writer = new ServiceWriterImpl(); //useful for testing correctness
-        String workingDir = System.getProperty("user.dir");
-        testFolders = new ArrayList<String>();
-        testFolders.add(workingDir + TEST_RESOURCES_PATH + OWLS_TC3_SERVICES);
+        testFolders = new ArrayList<URI>();
+        testFolders.add(ServiceReaderImplTest.class.getResource(OWLS_TC3_SERVICES).toURI());
 
-        ttlFilter = new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return (name.endsWith(SYNTAX.getExtension())) ;
-            }
-        };
+        ttlFilter = new FilenameFilterBySyntax(SYNTAX);
     }
 
     @Test
@@ -71,7 +66,7 @@ public class ServiceReaderImplTest {
 
         // Add all the test collections
         System.out.println("Reading test collections");
-        for (String testFolder : testFolders) {
+        for (URI testFolder : testFolders) {
             File dir = new File(testFolder);
             System.out.println("Test collection: " + testFolder);
 
@@ -85,7 +80,7 @@ public class ServiceReaderImplTest {
                     System.out.println("Parsing file: " + file.getName());
 
                     in = new FileInputStream(file);
-                    services = reader.parse(in,"TTL");
+                    services = reader.parse(in, "TTL");
                     // Run checks
                     Assert.assertNotNull("Service collection should not be null", services);
                     Assert.assertEquals(1, services.size());
@@ -98,19 +93,11 @@ public class ServiceReaderImplTest {
                     Model generatedModel = null;
                     Model originalModel = null;
                     try {
-                        generatedModel = ((ServiceWriterImpl)writer).generateModel(services.get(0));
+                        generatedModel = ((ServiceWriterImpl) writer).generateModel(services.get(0));
                         originalModel = ModelFactory.createDefaultModel();
                         // Reopen since reset is not directly supported
                         in = new FileInputStream(file);
                         originalModel.read(in, null, SYNTAX.getName());
-
-                        System.out.println("Original Model");
-                        originalModel.setNsPrefixes(Vocabularies.prefixes);
-                        originalModel.write(System.out, SYNTAX.getName());
-
-                        System.out.println("Model Read");
-                        generatedModel.setNsPrefixes(Vocabularies.prefixes);
-                        generatedModel.write(System.out, SYNTAX.getName());
 
                         Assert.assertTrue("The two RDF models are not isomorphic", generatedModel.isIsomorphicWith(originalModel));
                     } finally {
