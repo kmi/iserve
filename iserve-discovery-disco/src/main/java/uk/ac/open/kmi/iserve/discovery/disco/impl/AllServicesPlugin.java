0c4497027e9c7f5209c58e83e25118ed91fc9c58
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-06-16 13:09:50 +0100

    Ported AllServices plugin, and added tests

--- iserve-discovery-disco/src/main/java/uk/ac/open/kmi/iserve/discovery/disco/impl/AllServicesPlugin.java
+++ iserve-discovery-disco/src/main/java/uk/ac/open/kmi/iserve/discovery/disco/impl/AllServicesPlugin.java
@@ -1,221 +1,214 @@
 /*
-   Copyright ${year}  Knowledge Media Institute - The Open University
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ * Copyright (c) 2013. Knowledge Media Institute - The Open University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package uk.ac.open.kmi.iserve.discovery.disco.impl;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.ws.rs.core.MultivaluedMap;
-
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import uk.ac.open.kmi.iserve.commons.vocabulary.MSM;
 import uk.ac.open.kmi.iserve.discovery.api.DiscoveryException;
 import uk.ac.open.kmi.iserve.discovery.api.MatchResult;
 import uk.ac.open.kmi.iserve.discovery.api.OperationDiscoveryPlugin;
 import uk.ac.open.kmi.iserve.discovery.api.ServiceDiscoveryPlugin;
 import uk.ac.open.kmi.iserve.discovery.disco.DiscoMatchType;
 import uk.ac.open.kmi.iserve.discovery.disco.MatchResultImpl;
-import uk.ac.open.kmi.iserve.discovery.disco.Util;
 import uk.ac.open.kmi.iserve.sal.manager.impl.ManagerSingleton;
 
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.Resource;
+import javax.ws.rs.core.MultivaluedMap;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AllServicesPlugin implements ServiceDiscoveryPlugin, OperationDiscoveryPlugin {
 
-	private static final String PLUGIN_NAME = "all-jena";
+    private static final String PLUGIN_NAME = "all-jena";
 
-	private static final String PLUGIN_DESCRIPTION = "iServe trivial discovery. " +
-			"Returns all the services or operations available.";
+    private static final String PLUGIN_DESCRIPTION = "iServe trivial discovery. " +
+            "Returns all the services or operations available.";
 
-	private static final String PLUGIN_VERSION = "v0.2";
+    private static final String PLUGIN_VERSION = "v0.2";
 
-	// No Discovery Pluging Parameters	
-	private static final Map<String, String> parameterDetails = new HashMap<String, String>();
+    // No Discovery Pluging Parameters
+    private static final Map<String, String> parameterDetails = new HashMap<String, String>();
 
-	private static final Logger log = LoggerFactory.getLogger(AllServicesPlugin.class);
+    private static final Logger log = LoggerFactory.getLogger(AllServicesPlugin.class);
 
-	public static String NEW_LINE = System.getProperty("line.separator");
+    public static String NEW_LINE = System.getProperty("line.separator");
 
-	private int count;
+    private int count;
 
-	private URI sparqlEndpoint;
+    private URI sparqlEndpoint;
 
-	public AllServicesPlugin() {
-		sparqlEndpoint = ManagerSingleton.getInstance().getConfiguration().getDataSparqlUri();
-		if (sparqlEndpoint == null) {
-			log.error("The '" + this.getName() + "' " + this.getVersion() + 
-					" services discovery plugin currently requires a SPARQL endpoint.");
-			// TODO: Disable plugin
-		}
-	}
+    public AllServicesPlugin() {
+        sparqlEndpoint = ManagerSingleton.getInstance().getConfiguration().getDataSparqlUri();
+        if (sparqlEndpoint == null) {
+            log.error("The '" + this.getName() + "' " + this.getVersion() +
+                    " services discovery plugin currently requires a SPARQL endpoint.");
+            // TODO: Disable plugin
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getName()
-	 */
-	public String getName() {
-		return PLUGIN_NAME;
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getName()
+     */
+    public String getName() {
+        return PLUGIN_NAME;
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getDescription()
-	 */
-	public String getDescription() {
-		return PLUGIN_DESCRIPTION;
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getDescription()
+     */
+    public String getDescription() {
+        return PLUGIN_DESCRIPTION;
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getFeedTitle()
-	 */
-	public String getFeedTitle() {
-		String feedTitle = "all " + count + " known service(s)";
-		return feedTitle;
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getFeedTitle()
+     */
+    public String getFeedTitle() {
+        String feedTitle = "all " + count + " known service(s)";
+        return feedTitle;
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.IServiceDiscoveryPlugin#getVersion()
-	 */
-	@Override
-	public String getVersion() {
-		return PLUGIN_VERSION;
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.IServiceDiscoveryPlugin#getVersion()
+     */
+    @Override
+    public String getVersion() {
+        return PLUGIN_VERSION;
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getParametersDetails()
-	 */
-	@Override
-	public Map<String, String> getParametersDetails() {
-		return parameterDetails;
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.DiscoveryPlugin#getParametersDetails()
+     */
+    @Override
+    public Map<String, String> getParametersDetails() {
+        return parameterDetails;
+    }
 
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.ServiceDiscoveryPlugin#discoverServices(javax.ws.rs.core.MultivaluedMap)
-	 */
-	public Map<URL, MatchResult> discoverServices(MultivaluedMap<String, String> parameters) throws DiscoveryException {
-		return discover(false, parameters);
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.ServiceDiscoveryPlugin#discoverServices(javax.ws.rs.core.MultivaluedMap)
+     */
+    public Map<URL, MatchResult> discoverServices(MultivaluedMap<String, String> parameters) throws DiscoveryException {
+        return discover(false, parameters);
+    }
 
-	/* (non-Javadoc)
-	 * @see uk.ac.open.kmi.iserve.discovery.api.OperationDiscoveryPlugin#discoverOperations(javax.ws.rs.core.MultivaluedMap)
-	 */
-	@Override
-	public Map<URL, MatchResult> discoverOperations(MultivaluedMap<String, String> parameters) throws DiscoveryException {
-		return discover(true, parameters);
-	}
+    /* (non-Javadoc)
+     * @see uk.ac.open.kmi.iserve.discovery.api.OperationDiscoveryPlugin#discoverOperations(javax.ws.rs.core.MultivaluedMap)
+     */
+    @Override
+    public Map<URL, MatchResult> discoverOperations(MultivaluedMap<String, String> parameters) throws DiscoveryException {
+        return discover(true, parameters);
+    }
 
-	public Map<URL, MatchResult> discover(boolean operationDiscovery, MultivaluedMap<String, String> parameters) throws DiscoveryException {
-		
-		if (sparqlEndpoint == null) {
-			log.error("Unable to perform discovery, no SPARQL endpoint available.");
-			throw new DiscoveryException(403, "Unable to perform discovery, no SPARQL endpoint available.");
-		}
-		
-		log.debug("Discover services: " + parameters);
+    public Map<URL, MatchResult> discover(boolean operationDiscovery, MultivaluedMap<String, String> parameters) throws DiscoveryException {
 
-		// Matching results
-		Map<URL, MatchResult> results = new HashMap<URL, MatchResult>();
+        if (sparqlEndpoint == null) {
+            log.error("Unable to perform discovery, no SPARQL endpoint available.");
+            throw new DiscoveryException(403, "Unable to perform discovery, no SPARQL endpoint available.");
+        }
 
-		// Query for every item
-		StringBuffer queryBuffer = new StringBuffer(Util.generateQueryPrefix());
-		queryBuffer.append("select ?svc ?labelSvc ");
-		if (operationDiscovery) {
-			queryBuffer.append("?op ?labelOp");
-		}
-		queryBuffer.append(NEW_LINE);
-		queryBuffer.append("where {  ?svc a msm:Service ." + NEW_LINE);
+        log.debug("Discover services: " + parameters);
 
-		if (operationDiscovery) {
-			queryBuffer.append("?svc msm:hasOperation ?op ." + NEW_LINE);
-			queryBuffer.append("?op a msm:Operation ." + NEW_LINE);
-		} 
-		queryBuffer.append("  optional { ?svc rdfs:label ?labelSvc }" + NEW_LINE);
-		if (operationDiscovery) {
-			queryBuffer.append("  optional { ?op rdfs:label ?labelOp }" + NEW_LINE);
-		}
-		queryBuffer.append("}");
+        // Matching results
+        Map<URL, MatchResult> results = new HashMap<URL, MatchResult>();
 
-		log.info("Querying the backend: " + sparqlEndpoint + NEW_LINE + queryBuffer);
+        // Query for every item
+        StringBuilder queryBuffer = new StringBuilder().
+                append("select ?svc ?labelSvc ");
+        if (operationDiscovery) {
+            queryBuffer.append("?op ?labelOp");
+        }
+        queryBuffer.append(NEW_LINE);
+        queryBuffer.append("where {  ?svc a <" + MSM.Service.getURI() + "> ." + NEW_LINE);
 
-		// Query the engine
-		Query query = QueryFactory.create(queryBuffer.toString());
-		QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpoint.toASCIIString(), query);
-		
-		URL matchUrl = null;
-		String matchLabel = null;
-		Resource resource = null;
-		Literal labelResource = null;
-		String matchBinding = null;
-		String labelBinding = null;
+        if (operationDiscovery) {
+            queryBuffer.append("?svc <" + MSM.hasOperation.getURI() + "> ?op ." + NEW_LINE).
+                    append("?op a <" + MSM.Operation.getURI() + "> ." + NEW_LINE);
+        }
+        queryBuffer.append("  optional { ?svc <" + RDFS.label.getURI() + "> ?labelSvc }" + NEW_LINE);
+        if (operationDiscovery) {
+            queryBuffer.append("  optional { ?op <" + RDFS.label.getURI() + "> ?labelOp }" + NEW_LINE);
+        }
+        queryBuffer.append("}");
 
-		if (operationDiscovery) {
-			matchBinding = "op";
-			labelBinding = "labelOp";
-		} else {
-			matchBinding = "svc";
-			labelBinding = "labelSvc";
-		}
-		
-		try {		
-			ResultSet qResults = qexec.execSelect();
-			while ( qResults.hasNext() ) {
-				QuerySolution soln = qResults.nextSolution();
+        log.debug("Querying the backend: " + sparqlEndpoint + NEW_LINE + queryBuffer);
 
-				// Get the match URL
-				resource = soln.getResource(matchBinding);
-				if (resource != null && resource.isURIResource()) {
-					matchUrl = new URL(resource.getURI());
-				}
+        // Query the engine
+        Query query = QueryFactory.create(queryBuffer.toString());
+        QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpoint.toASCIIString(), query);
 
-				// Get label if it exists
-				labelResource = soln.getLiteral(labelBinding);
-				if (labelResource != null) {
-					matchLabel = labelResource.toString();
-				}
+        URL matchUrl = null;
+        String matchLabel = null;
+        Resource resource;
+        Literal labelResource;
+        String matchBinding;
+        String labelBinding;
 
-				if (matchUrl != null) {
-					// Ensure we got a result before proceeding further
-					// Create a match result 
-					MatchResultImpl match = new MatchResultImpl(matchUrl, matchLabel);
-					match.setMatchType(DiscoMatchType.EXACT);
-					match.setScore(Float.valueOf(0));
-					// TODO: Add these
-					// match.setEngineUrl(engineUrl);
-					// match.setRequest(request);
-					// Add the result
-					results.put(match.getMatchUrl(), match);
-				}
-			}
-		} catch (MalformedURLException e) {
-			log.error("Error obtaining match result. Expected a correct URL", e);
-		} finally {
-			qexec.close();
-		}
-	
-		log.debug("Matching results: " + results);
-		return results;
-	}
+        if (operationDiscovery) {
+            matchBinding = "op";
+            labelBinding = "labelOp";
+        } else {
+            matchBinding = "svc";
+            labelBinding = "labelSvc";
+        }
+
+        try {
+            ResultSet qResults = qexec.execSelect();
+            while (qResults.hasNext()) {
+                QuerySolution soln = qResults.nextSolution();
+
+                // Get the match URL
+                resource = soln.getResource(matchBinding);
+                if (resource != null && resource.isURIResource()) {
+                    matchUrl = new URL(resource.getURI());
+                }
+
+                // Get label if it exists
+                labelResource = soln.getLiteral(labelBinding);
+                if (labelResource != null) {
+                    matchLabel = labelResource.toString();
+                }
+
+                if (matchUrl != null) {
+                    // Ensure we got a result before proceeding further
+                    // Create a match result
+                    MatchResultImpl match = new MatchResultImpl(matchUrl, matchLabel);
+                    match.setMatchType(DiscoMatchType.EXACT);
+                    match.setScore(Float.valueOf(0));
+                    // TODO: Add these
+                    // match.setEngineUrl(engineUrl);
+                    // match.setRequest(request);
+                    // Add the result
+                    results.put(match.getMatchUrl(), match);
+                }
+            }
+        } catch (MalformedURLException e) {
+            log.error("Error obtaining match result. Expected a correct URL", e);
+        } finally {
+            qexec.close();
+        }
+
+        log.debug("Matching results: " + results);
+        return results;
+    }
 }
---
+++ iserve-discovery-disco/src/test/java/uk/ac/open/kmi/iserve/discovery/disco/impl/AllServicesPluginTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2013. Knowledge Media Institute - The Open University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.open.kmi.iserve.discovery.disco.impl;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.open.kmi.iserve.commons.io.FilenameFilterBySyntax;
+import uk.ac.open.kmi.iserve.commons.io.Syntax;
+import uk.ac.open.kmi.iserve.commons.model.Service;
+import uk.ac.open.kmi.iserve.discovery.api.MatchResult;
+import uk.ac.open.kmi.iserve.sal.ServiceFormat;
+import uk.ac.open.kmi.iserve.sal.manager.impl.ManagerSingleton;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * AllServicesPluginTest
+ * Tests for uploading services to the server should be cleared for these tests
+ * <p/>
+ * Author: Carlos Pedrinaci (KMi - The Open University)
+ * Date: 15/06/2013
+ * Time: 18:01
+ */
+public class AllServicesPluginTest {
+
+    private static final String OWLS_TC_SERVICES = "/OWLS-TC3-MSM";
+    private static final String JGD_SERVICES = "/jgd-services";
+    private static final String SOA4RE_SERVICES = "/soa4re";
+
+    private URI testFolder;
+
+    private File[] msmTtlTcFiles;
+
+    private AllServicesPlugin plugin;
+
+    private int numServices = 0;
+    private int numOperations = 0;
+
+    @Before
+    public void setUp() throws Exception {
+
+        // Clean the whole thing before testing
+        ManagerSingleton.getInstance().clearRegistry();
+
+        testFolder = AllServicesPluginTest.class.getResource(OWLS_TC_SERVICES).toURI();
+        FilenameFilter ttlFilter = new FilenameFilterBySyntax(Syntax.TTL);
+        File dir = new File(testFolder);
+        msmTtlTcFiles = dir.listFiles(ttlFilter);
+
+        URI uri;
+        Service svc;
+        FileInputStream in;
+        // Upload every document and obtain their URLs
+        for (File ttlFile : msmTtlTcFiles) {
+            in = new FileInputStream(ttlFile);
+            uri = ManagerSingleton.getInstance().importService(in, ServiceFormat.MSM_TTL);
+            svc = ManagerSingleton.getInstance().getService(uri);
+            numServices++;
+            numOperations += svc.getOperations().size();
+        }
+
+        plugin = new AllServicesPlugin();
+    }
+
+    @Test
+    public void testDiscoverServices() throws Exception {
+        Map<URL, MatchResult> result = plugin.discoverServices(null);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(numServices, result.size());
+    }
+
+    @Test
+    public void testDiscoverOperations() throws Exception {
+        Map<URL, MatchResult> result = plugin.discoverOperations(null);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(numOperations, result.size());
+    }
+}
