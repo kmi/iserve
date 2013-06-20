60a7b5f2a2509c07eff6d75ef7f242b73440d9d0
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-06-20 17:58:18 +0100

    URI Util was not retrieving the ID correctly. Fixed and implemented corresponding test

--- iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/util/UriUtil.java
+++ iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/util/UriUtil.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.ac.open.kmi.iserve.sal.util;
 
 import java.net.URI;
@@ -27,19 +28,34 @@ public class UriUtil {
 
     /**
      * Given the URI of a resource it returns the Unique ID.
-     * Checks for those that are local to the server only
+     * In reality this obtains the first path element after the basePath.
+     * Checks for those that are local to the server only. Developed both for hash and slash URIs.
+     * <p/>
+     * Works in iServe on the basis of the following assumption behind resource URIs:
+     * <p/>
+     * http://host:port/...some...path.../{uniqueResourceId}#fragment
+     * or
+     * http://host:port/...some...path.../{uniqueResourceId}/subPart
+     * <p/>
+     * The result should be {uniqueResourceId}
      *
-     * @param resourceUri
+     * @param resourceUri the actual resource URI
      * @param basePath    the base path of the URI
      * @return the uniqueId if it is a local URI to the server or null.
      */
     public static String getUniqueId(URI resourceUri, URI basePath) {
         String result = null;
-        URI relativeUri = resourceUri.relativize(basePath);
+        URI relativeUri = basePath.relativize(resourceUri);
         // If it is relative the first part of the path is the Unique ID
         if (!relativeUri.isAbsolute()) {
-            result = relativeUri.toString().substring(0,
-                    relativeUri.toString().indexOf('/'));
+            result = relativeUri.toASCIIString();
+            int slashIndex = result.indexOf('/');
+            int hashIndex = result.indexOf('#');
+            if (slashIndex > -1) {
+                result = result.substring(0, slashIndex);
+            } else if (hashIndex > -1) {
+                result = result.substring(0, hashIndex);
+            }
         }
         return result;
     }
