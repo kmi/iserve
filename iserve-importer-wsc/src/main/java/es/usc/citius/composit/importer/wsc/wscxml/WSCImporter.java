59aed109aaa95ea8243c9a1e66f9aac79f8d1ca9
Pablo <pablo.rodriguez.mier@gmail.com>
2013-07-22 11:42:49 +0100

    Minor changes on WSCImporter / WSCXMLSemanticReasoner

--- iserve-importer-wsc/src/main/java/es/usc/citius/composit/importer/wsc/wscxml/WSCImporter.java
+++ iserve-importer-wsc/src/main/java/es/usc/citius/composit/importer/wsc/wscxml/WSCImporter.java
@@ -1,14 +1,12 @@
 package es.usc.citius.composit.importer.wsc.wscxml;
 
 
-
-
-
 import es.usc.citius.composit.importer.wsc.wscxml.model.XMLInstance;
 import es.usc.citius.composit.importer.wsc.wscxml.model.services.XMLService;
 import es.usc.citius.composit.importer.wsc.wscxml.model.services.XMLServices;
 import uk.ac.open.kmi.iserve.commons.model.MessageContent;
 import uk.ac.open.kmi.iserve.commons.model.Operation;
+import uk.ac.open.kmi.iserve.commons.model.Resource;
 import uk.ac.open.kmi.iserve.commons.model.Service;
 import uk.ac.open.kmi.iserve.sal.ServiceImporter;
 import uk.ac.open.kmi.iserve.sal.exception.ImporterException;
@@ -16,21 +14,37 @@ import uk.ac.open.kmi.iserve.sal.exception.ImporterException;
 import javax.xml.bind.JAXB;
 import java.io.*;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Imports and transforms datasets from the Web Service Challenge 2008 (XML format)
- * User: Pablo Rodriguez Mier
  * Date: 7/18/13
- * Time: 5:04 PM
+ * @author Pablo Rodríguez Mier
  */
 public class WSCImporter implements ServiceImporter {
 
     public static final String DEFAULT_NAMESPACE_URI = "http://www.ws-challenge.org/WSC08Services";
+    private WSCXMLSemanticReasoner reasoner;
+    private String xmlTaxonomyURL;
+    private String ontologyName = "taxonomy.owl";
+    private String owlTaxonomyURL;
 
-    public WSCImporter(){
+    /**
+     * By default, the WSC specification does not define the ontology URI of the concepts.
+     * Thus, the real URI of the ontology should be provided. Example: http://localhost/ontology/taxonomy.xml
+     * @param xmlTaxonomyURL URL of the ontology where the semantic concepts (inputs/outputs) are defined (XML format)
+     */
+    public WSCImporter(String xmlTaxonomyURL) throws IOException {
+        // NOTE: The inputs and outputs of the services should be automatically translated from instances to
+        // concepts using the XML Reasoner
+        URL ontology = new URL(xmlTaxonomyURL);
+        this.reasoner = new WSCXMLSemanticReasoner(ontology.openStream());
+        // TODO Convert the ontology to OWL ??
 
+        this.xmlTaxonomyURL = xmlTaxonomyURL;
     }
 
     @Override
@@ -38,55 +52,43 @@ public class WSCImporter implements ServiceImporter {
         return transform(originalDescription, null);
     }
 
+    private MessageContent transform(XMLInstance instance, String baseURI){
+        String concept = this.reasoner.getConceptInstance(instance.getName());
+        URI uri = URI.create(baseURI + ontologyName + "#" + concept);
+        MessageContent content = new MessageContent(uri);
+        content.setSource(uri);
+        content.setWsdlGrounding(uri);
+        content.setLabel(instance.getName());
+        content.addModelReference(new Resource(uri));
+        return content;
+    }
+
     @Override
     public List<Service> transform(InputStream originalDescription, String baseUri) throws ImporterException {
-        // Deserialize
+        // De-serialize from XML
         XMLServices services = JAXB.unmarshal(originalDescription, XMLServices.class);
-        // Create the services following the iserve-commons-vocabulary
+        List<Service> listServices = new ArrayList<Service>(services.getServices().size());
+        String uri = baseUri.endsWith("/") ? baseUri : baseUri + "/";
+        // Create the services following the iserve-commons-vocabulary model
         for(XMLService service : services.getServices()){
-            URI srvURI = URI.create(DEFAULT_NAMESPACE_URI);
+            URI srvURI = URI.create(uri+service.getName());
             Service modelService = new Service(srvURI);
             modelService.setSource(srvURI);
             modelService.setWsdlGrounding(srvURI);
 
-            //modelService.setLabel();
-            Operation operation = new Operation(URI.create(DEFAULT_NAMESPACE_URI + "/" + service.getName()));
+            Operation operation = new Operation(URI.create(baseUri + service.getName()));
             for(XMLInstance input : service.getInputs().getInstances()){
-                MessageContent inputContent = new MessageContent(URI.create(DEFAULT_NAMESPACE_URI + "/" + input.getName()));
-                //inputContent.
-                operation.addInput(inputContent);
+                operation.addInput(transform(input, baseUri));
             }
             for(XMLInstance output : service.getOutputs().getInstances()){
-
+                operation.addOutput(transform(output, baseUri));
             }
             modelService.addOperation(operation);
+            modelService.setLabel(service.getName());
+
+            listServices.add(modelService);
         }
-        /*
-        msmSvc.setSource(svcUri);
-        msmSvc.setWsdlGrounding(svcUri);
-        msmSvc.setLabel(qname.getLocalPart());
-
-        // Add documentation
-        addComment(wsdlSvc, msmSvc);
-
-        addModelReferences(wsdlSvc, msmSvc);
-
-        // Process Operations
-        URI baseUri;
-        uk.ac.open.kmi.iserve.commons.model.Operation msmOp;
-        Port[] ports = wsdlSvc.getPorts();
-        for (Port port : ports) {
-            if (port.hasBinding()) {
-                BindingOperation[] operations = port.findBinding().getOperations();
-                for (BindingOperation operation : operations) {
-                    msmOp = transform(operation,xplore  URIUtil.getNameSpace(svcUri), port.getName());
-                    msmSvc.addOperation(msmOp);
-                }
-
-            }
-        }*/
-
-        return null;
+        return listServices;
     }
 
     @Override
@@ -111,12 +113,17 @@ public class WSCImporter implements ServiceImporter {
                 }
             }
         }
-        // Return an empty array if it could not be transformed.
-        return new ArrayList<Service>();
+        return Collections.emptyList();
     }
 
     @Override
     public List<Service> transform(File originalDescription, String baseUri) throws ImporterException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return transform(originalDescription);
     }
+
+    /*
+    public static void main(String[] args) throws IOException, ImporterException {
+        WSCImporter imp = new WSCImporter(new File("/home/citius/KMi/iserve-project/http-server/htdocs/ontology/taxonomy.xml").toURI().toURL().toString());
+        imp.transform(new FileInputStream(new File("/home/citius/KMi/iserve-project/http-server/htdocs/services/services.xml")),"");
+    }*/
 }
