877be8759a1f595ea4d8d921370b12f460b32dea
Pablo <pablo.rodriguez.mier@gmail.com>
2013-07-24 17:10:57 +0100

    Matcher (empty) interface created

---
+++ iserve-discovery-api/src/main/java/uk/ac/open/kmi/iserve/discovery/api/Matcher.java
@@ -0,0 +1,10 @@
+package uk.ac.open.kmi.iserve.discovery.api;
+
+/**
+ *
+ * @author Carlos Pedrinaci (Knowledge Media Institute - The Open University)
+ * @author Pablo Rodriguez Mier (CITIUS - Universidad de Santiago de Compostela)
+ */
+public interface Matcher {
+
+}
