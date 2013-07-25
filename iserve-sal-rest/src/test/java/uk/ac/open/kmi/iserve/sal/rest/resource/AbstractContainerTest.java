6fac520c349e5b31326de6beb50147368e864daf
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-07-25 16:37:34 +0100

    Started moving to Jetty 8 for testing. Issues with JSTLs remain though. SAL REST tests won't work for now

--- iserve-sal-rest/src/test/java/uk/ac/open/kmi/iserve/sal/rest/resource/AbstractContainerTest.java
+++ iserve-sal-rest/src/test/java/uk/ac/open/kmi/iserve/sal/rest/resource/AbstractContainerTest.java
@@ -1,30 +1,27 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright (c) 2013. Knowledge Media Institute - The Open University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package uk.ac.open.kmi.iserve.sal.rest.resource;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
 
 import static org.junit.Assert.assertTrue;
 
@@ -47,7 +44,9 @@ public abstract class AbstractContainerTest {
             Connector connector = new SelectChannelConnector();
             connector.setPort(port);
             server.setConnectors(new Connector[]{connector});
-            server.setHandler(new WebAppContext("src/main/webapp", "/iserve"));
+            WebAppContext webContext = new WebAppContext("src/main/webapp", "/iserve");
+            server.setHandler(webContext);
+
             server.start();
             assertTrue(server.isStarted());
         }
