aac8c343ce6f9f3350e1a752ec06812fae6fa333
Pablo <pablo.rodriguez.mier@gmail.com>
2013-09-19 15:52:18 +0100

    Moved hazelcast to a different module

--- iserve-discovery-hazelcast/src/main/java/uk/ac/open/kmi/iserve/discovert/hazelcast/HazelcastIndexedConceptMatcher.java
+++ iserve-discovery-hazelcast/src/main/java/uk/ac/open/kmi/iserve/discovert/hazelcast/HazelcastIndexedConceptMatcher.java
@@ -1,5 +1,6 @@
-package uk.ac.open.kmi.iserve.discovery.disco.hazelcast;
+package uk.ac.open.kmi.iserve.discovert.hazelcast;
 
+import com.google.common.collect.Table;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 import com.hazelcast.client.HazelcastClient;
@@ -14,11 +15,15 @@ import uk.ac.open.kmi.iserve.discovery.api.impl.AtomicMatchResult;
 import uk.ac.open.kmi.iserve.discovery.api.impl.EnumMatchTypes;
 import uk.ac.open.kmi.iserve.discovery.disco.DiscoMatchType;
 import uk.ac.open.kmi.iserve.discovery.disco.LogicConceptMatchType;
+import uk.ac.open.kmi.iserve.discovery.disco.impl.SparqlLogicConceptMatcher;
 import uk.ac.open.kmi.iserve.sal.exception.SalException;
 import uk.ac.open.kmi.iserve.sal.manager.IntegratedComponent;
+import uk.ac.open.kmi.iserve.sal.manager.iServeManager;
 
 import javax.inject.Named;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,21 +33,45 @@ import java.util.Set;
 public class HazelcastIndexedConceptMatcher extends IntegratedComponent implements Matcher {
 
     private IMap<URI, Map<URI, String>> map;
+    private iServeManager manager;
+    private SparqlLogicConceptMatcher matcher;
 
     @Inject
     protected HazelcastIndexedConceptMatcher(EventBus eventBus,
-                                             @Named("iserve.url") String iServeUri,
-                                             @Named("iserve.hazelcast.address") String addr // ip:port
-                                             ) throws SalException {
+                                             ClientConfig hazelcastClientConfig,
+                                             iServeManager iServeManager,
+                                             SparqlLogicConceptMatcher sparqlMatcher,
+                                             @Named("hazelcast.map.id") String hzMapId,
+                                             @Named("iserve.url") String iServeUri) throws SalException {
 
 
         super(eventBus, iServeUri);
         // Load config from properties? injected through the constructor?
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.addAddress(addr);  // config required
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        //ClientConfig clientConfig = new ClientConfig();
+        //clientConfig.addAddress(addr);  // config required
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(hazelcastClientConfig);
         // Request a new (default) map
-        this.map = client.getMap(this.getClass().getName());
+        this.map = client.getMap(hzMapId);
+        this.manager = iServeManager;
+        this.matcher = sparqlMatcher;
+        // Population strategy if the map is not populated correctly?
+        // NOTE: Automatic mechanism for updating the indexes continuously when changes are
+        // detected in the RDF store is required.
+        populate(map);
+    }
+
+    // TODO: This should not be here. A separate crawler is required to update the index
+    private void populate(IMap<URI,Map<URI, String>> map) {
+        Set<URI> classes = new HashSet<URI>(this.manager.getKnowledgeBaseManager().listConcepts(null));
+        Table<URI, URI, MatchResult> table = matcher.listMatchesAtLeastOfType(classes, LogicConceptMatchType.Plugin);
+        for(URI origin : table.rowKeySet()){
+            Map<URI, String> destMatch = new HashMap<URI, String>();
+            Map<URI, MatchResult> dest = table.row(origin);
+            for(URI destUri : dest.keySet()){
+                destMatch.put(destUri, dest.get(destUri).getMatchType().name());
+            }
+            map.put(origin, destMatch);
+        }
     }
 
     @Override
@@ -52,7 +81,7 @@ public class HazelcastIndexedConceptMatcher extends IntegratedComponent implemen
 
     @Override
     public String getMatcherVersion() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return "HazelCast indexed matcher, v0.1";
     }
 
     @Override
