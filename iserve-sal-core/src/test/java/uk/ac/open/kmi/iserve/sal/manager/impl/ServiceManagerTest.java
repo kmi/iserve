7b8be205a72d0a22ceec58d408307917919178a9
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2013-10-15 12:25:22 +0100

    Reshaped tests so that they obtain the configuration properties from the proper file. Additionally we use Jukito for handling Guice. We should also be able to test all implementations of any interface easily too.

--- iserve-sal-core/src/test/java/uk/ac/open/kmi/iserve/sal/manager/impl/ServiceManagerTest.java
+++ iserve-sal-core/src/test/java/uk/ac/open/kmi/iserve/sal/manager/impl/ServiceManagerTest.java
@@ -16,14 +16,13 @@
 
 package uk.ac.open.kmi.iserve.sal.manager.impl;
 
-import com.google.common.eventbus.EventBus;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 import junit.framework.Assert;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.jukito.JukitoRunner;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.open.kmi.iserve.commons.io.TransformationException;
@@ -31,8 +30,11 @@ import uk.ac.open.kmi.iserve.commons.io.Transformer;
 import uk.ac.open.kmi.iserve.commons.model.Service;
 import uk.ac.open.kmi.iserve.sal.exception.ServiceException;
 import uk.ac.open.kmi.iserve.sal.manager.ServiceManager;
+import uk.ac.open.kmi.iserve.sal.manager.SparqlGraphStoreManager;
 
+import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -42,41 +44,118 @@ import java.util.Set;
 
 import static org.junit.Assert.*;
 
-
 /**
- * ServiceManagerRdf test
+ * ServiceManagerSparqlTest
+ * TODO: Provide Description
+ *
+ * @author <a href="mailto:carlos.pedrinaci@open.ac.uk">Carlos Pedrinaci</a> (KMi - The Open University)
+ * @since 11/10/2013
  */
-public class ServiceManagerRdfTest {
+@RunWith(JukitoRunner.class)
+public class ServiceManagerTest {
 
-    private static final Logger log = LoggerFactory.getLogger(ServiceManagerRdfTest.class);
-    private static final String DATASET = "/WSC08/wsc08_datasets/01/";
+    private static final Logger log = LoggerFactory.getLogger(ServiceManagerTest.class);
 
     private static final String MEDIATYPE = "text/xml";
 
-    //TODO: Change this to be read from a configuration file
-    private static final String ISERVE_TEST_URI = "http://localhost:9090/iserve";
-    private static final String ISERVE_TEST_QUERY_URI = "http://localhost:8080/openrdf-sesame/repositories/Test";
-    private static final String ISERVE_TEST_UPDATE_URI = "http://localhost:8080/openrdf-sesame/repositories/Test/statements";
-    private static final String ISERVE_TEST_SERVICE_URI = "http://localhost:8080/openrdf-sesame/repositories/Test/rdf-graphs/service";
+    private static final String WSC08_01 = "/WSC08/wsc08_datasets/01/";
+    private static final String WSC08_01_SERVICES = WSC08_01 + "services.xml";
 
+    @Inject
+    ServiceManager serviceManager;
 
-    private static ServiceManager serviceManager;
+    /**
+     * JukitoModule.
+     */
+    public static class InnerModule extends ConfiguredTestModule {
+        @Override
+        protected void configureTest() {
+            // Get properties
+            super.configureTest();
 
+            // bind
+            bind(ServiceManager.class).to(ServiceManagerSparql.class);
+
+            // Necessary to verify interaction with the real object
+            bindSpy(ServiceManagerSparql.class);
+
+            // Assisted Injection for the Graph Store Manager
+            install(new FactoryModuleBuilder()
+                    .implement(SparqlGraphStoreManager.class, ConcurrentSparqlGraphStoreManager.class)
+                    .build(SparqlGraphStoreFactory.class));
+        }
+    }
+
+    // One of setup based on the use of a file to indicate if we need to initialise
+    // Can't be easily done otherwise since the test is injected every time
     @BeforeClass
-    public static void setUp() throws Exception {
+    public static void oneOfSetup() throws IOException, URISyntaxException {
+        URL resource = ServiceManagerTest.class.getResource(".");
+        File folder = new File(resource.toURI());
+        if (folder.exists() && folder.isDirectory() && folder.canWrite()) {
+            File file = new File(folder.getAbsolutePath() + "/init.check");
+            if (!file.exists())
+                file.createNewFile();
+        }
+    }
+
+    // One of setup based on the use of a file to indicate if we need to initialise
+    // Can't be easily done otherwise since the test is injected every time
+    @AfterClass
+    public static void oneOfTearDown() throws URISyntaxException {
+        URL resource = ServiceManagerTest.class.getResource("init.check");
+        if (resource != null) {
+            File file = new File(resource.toURI());
+            if (file != null && file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
         BasicConfigurator.configure();
         org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
 
-        Injector injector = Guice.createInjector(new iServeManagementModule());
-        EventBus eventBus = injector.getInstance(EventBus.class);
-        SparqlGraphStoreFactory factory = injector.getInstance(SparqlGraphStoreFactory.class);
-
-        serviceManager = new ServiceManagerSparql(eventBus, factory, ISERVE_TEST_URI, ISERVE_TEST_QUERY_URI, ISERVE_TEST_UPDATE_URI, ISERVE_TEST_SERVICE_URI);
-
-        serviceManager.clearServices();
-        importWscServices();
+        URL resource = ServiceManagerTest.class.getResource("init.check");
+        if (resource != null) {
+            File file = new File(resource.toURI());
+            if (file.exists()) {
+                serviceManager.clearServices();
+                importWscServices();
+                file.delete();
+            }
+        }
     }
 
+    @After
+    public void tearDown() throws Exception {
+        serviceManager.shutdown();
+    }
+
+    private void importWscServices() throws TransformationException, ServiceException, URISyntaxException {
+        log.info("Importing WSC Dataset");
+        String file = this.getClass().getResource(WSC08_01_SERVICES).getFile();
+        log.info("Services XML file {}", file);
+        File servicesFile = new File(file);
+        URL base = this.getClass().getResource(WSC08_01);
+        log.info("Dataset Base URI {}", base.toURI().toASCIIString());
+        List<Service> services = Transformer.getInstance().transform(servicesFile, base.toURI().toASCIIString(), MEDIATYPE);
+        //List<Service> result = Transformer.getInstance().transform(services, null, MEDIATYPE);
+        if (services.size() == 0) {
+            fail("No services transformed!");
+        }
+        // Import all services
+        int counter = 0;
+        for (Service svc : services) {
+            URI uri = serviceManager.addService(svc);
+            Assert.assertNotNull(uri);
+            log.info("Service added: " + uri.toASCIIString());
+            counter++;
+        }
+        log.debug("Total services added {}", counter);
+        Assert.assertEquals(services.size(), counter);
+    }
 
     @Test
     public void testListServices() throws Exception {
@@ -85,31 +164,9 @@ public class ServiceManagerRdfTest {
         // Check the original list of retrieved URIs
         assertEquals(158, services.size());
         // Check if there are no duplications
-        assertEquals(158, new HashSet<URI>(services).size());
+        assertEquals(new HashSet<URI>(services).size(), services.size());
     }
 
-    private static void importWscServices() throws TransformationException, ServiceException, URISyntaxException {
-        log.info("Importing WSC Datasets");
-        String file = ServiceManagerRdfTest.class.getResource(DATASET + "services.xml").getFile();
-        log.info("Services XML file {}", file);
-        File services = new File(file);
-        URL base = ServiceManagerRdfTest.class.getResource(DATASET);
-        log.info("Dataset Base URI {}", base.toURI().toASCIIString());
-        List<Service> result = Transformer.getInstance().transform(services, base.toURI().toASCIIString(), MEDIATYPE);
-        //List<Service> result = Transformer.getInstance().transform(services, null, MEDIATYPE);
-        if (result.size() == 0) {
-            fail("No services transformed!");
-        }
-        // Import all services
-        int counter = 0;
-        for (Service s : result) {
-            URI uri = serviceManager.addService(s);
-            Assert.assertNotNull(uri);
-            log.info("Service added: " + uri.toASCIIString());
-            counter++;
-        }
-        log.debug("Total services added {}", counter);
-    }
 
     /**
      * Finds the URI with the first coincidence with the service name
@@ -297,4 +354,5 @@ public class ServiceManagerRdfTest {
         result = serviceManager.listServicesWithOutputType(URI.create("http://localhost/wsc/01/taxonomy.owl#con38363177"));
         assertEquals(1, result.size());
     }
+
 }
