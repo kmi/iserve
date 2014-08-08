3608ebacf412ef28b6444ea59fd1f7276dfda558
izzyblues <luca.panziera@open.ac.uk>
2014-08-08 15:40:52 +0100

    Defined interface for NFP-Manager

---
+++ iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/manager/NfpManager.java
@@ -0,0 +1,60 @@
+package uk.ac.open.kmi.iserve.sal.manager;
+
+import com.google.common.collect.Table;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Interface that defines the operations a NFP Manager should offer
+ * If other implementations backed by different types of storage systems are
+ * needed we should update this.
+ *
+ * @author <a href="mailto:carlos.pedrinaci@open.ac.uk">Carlos Pedrinaci</a> (KMi - The Open University)
+ * @author <a href="mailto:luca.panziera@open.ac.uk">Luca Panziera</a> (KMi - The Open University)
+ */
+public interface NfpManager extends iServeComponent {
+    // CREATE (return boolean or throw exception?)
+
+    public void createPropertyValue(URI resource, URI property, Object value);
+
+    public void createPropertyValuesOfResource(Map<URI, Object> propertyValueMap, URI resource);
+
+    public void createPropertyValueOfResources(Map<URI, Object> resourceValueMap, URI property);
+
+    // READ
+
+    public Object getPropertyValue(URI resource, URI property, Class valueClass);
+
+    public Map<URI, Object> getPropertyValueOfResources(Set<URI> resource, URI property, Class valueClass);
+
+    public Map<URI, Object> getPropertyValuesOfResource(URI resource, Map<URI, Class> propertyValueClassMap);
+
+    public Map<URI, Object> getAllPropertyValuesOfResource(URI resource);
+
+    public Table<URI, URI, Object> getPropertyValuesOfResources(Set<URI> resources, Map<URI, Class> propertyValueClassMap);
+
+    // UPDATE (return boolean or throw exception?)
+
+    public void updatePropertyValue(URI resource, URI property, Object value);
+
+    public void updatePropertyValuesOfResource(Map<URI, Object> propertyValueMap, URI resource);
+
+    public void updatePropertyValueOfResources(Map<URI, Object> resourceValueMap, URI property);
+
+    // DELETE (return boolean or throw exception?)
+
+    public void deletePropertyValue(URI resource, URI property);
+
+    public void deletePropertyValueOfResources(Set<URI> resources, URI property);
+
+    public void deletePropertyValuesOfResource(URI resource, Set<URI> properties);
+
+    public void deleteAllPropertyValuesOfResource(URI resource);
+
+    public void deleteAllValuesOfProperty(URI property);
+
+    public void clearPropertyValues();
+
+}
