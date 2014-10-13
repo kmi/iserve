def36d19cb1eecee0eaad61f40e071a6e2904857
izzyblues <luca.panziera@open.ac.uk>
2014-10-13 14:27:31 +0100

    Fixed RESTful APIs for service documents

--- iserve-elda/src/main/java/uk/ac/open/kmi/iserve/elda/EldaFilter.java
+++ iserve-elda/src/main/java/uk/ac/open/kmi/iserve/elda/EldaFilter.java
@@ -1,5 +1,8 @@
 package uk.ac.open.kmi.iserve.elda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +15,7 @@ public class EldaFilter implements Filter {
 
     private RequestDispatcher defaultRequestDispatcher;
     private RequestDispatcher salRestRequestDispatcher;
+    private Logger logger = LoggerFactory.getLogger(EldaFilter.class);
 
     @Override
     public void destroy() {
@@ -25,14 +29,18 @@ public class EldaFilter implements Filter {
         res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        if (httpRequest.getRequestURI().equals("/iserve/id/services")) {
+        if (httpRequest.getRequestURI().equals("/iserve/id/services") && httpRequest.getMethod().equalsIgnoreCase("GET")) {
             // TODO Fix this by debugging Elda config file
+            logger.debug("Redirection...");
             res.sendRedirect("/iserve/doc/services");
-        } else if (httpRequest.getRequestURI().matches("/iserve/id.*") && !httpRequest.getMethod().equalsIgnoreCase("GET")) {
+        } else if ((httpRequest.getRequestURI().matches("/iserve/id.*") && !httpRequest.getMethod().equalsIgnoreCase("GET")) || httpRequest.getRequestURI().matches("/iserve/id/documents.*")) {
+            logger.debug("Forward to SAL REST");
             salRestRequestDispatcher.forward(request, response);
         } else if ((httpRequest.getRequestURI().matches("/iserve/id.*") && httpRequest.getMethod().equalsIgnoreCase("GET")) || (httpRequest.getRequestURI().matches("/iserve/api-docs.*"))) {
+            logger.debug("Put message in the filer chain");
             chain.doFilter(request, response);
         } else {
+            logger.debug("Forward to default Servlet");
             defaultRequestDispatcher.forward(request, response);
         }
 
