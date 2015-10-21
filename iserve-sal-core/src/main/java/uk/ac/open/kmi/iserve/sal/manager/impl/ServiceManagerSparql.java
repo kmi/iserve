e214608da0d518f756bcae44aef955c6310851a7
Carlos Pedrinaci <c.pedrinaci@open.ac.uk>
2015-10-21 17:21:26 +0100

    Support hasPart(s) through variable length paths for systems with and without transitive properties. It slows down a bit systems that actually have support for transitive properties.

--- iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/manager/impl/ServiceManagerSparql.java
+++ iserve-sal-core/src/main/java/uk/ac/open/kmi/iserve/sal/manager/impl/ServiceManagerSparql.java
@@ -864,13 +864,17 @@ public class ServiceManagerSparql extends IntegratedComponent implements Service
             queryBuilder.append(" ?entity <").append(dataPropertyType.getURI()).append("> ?message .").append("\n");
         }
 
-        queryBuilder.append("{ ?message <").append(MSM.hasPartTransitive.getURI()).append("> ?part . }").append("\n");
+// TODO: Fix this bit
+// Important: This assumes the chains will always be mandatory or optional. If there are optional parts within
+// mandatory we won't get and vice versa. For those cases we'd really want reasoning
+// Remove these for now since we are now using variable length paths
+//        queryBuilder.append("{ ?message <").append(MSM.hasPartTransitive.getURI()).append("> ?part . }").append("\n");
+//        queryBuilder.append(" UNION \n");
+//        queryBuilder.append("{ ?message <").append(MSM.hasPart.getURI()).append("> ?part . }").append("\n");
+//        queryBuilder.append(" UNION \n");
+        queryBuilder.append("{ ?message <").append(MSM.hasOptionalPart.getURI()).append(">+ ?part . }").append("\n");
         queryBuilder.append(" UNION \n");
-        queryBuilder.append("{ ?message <").append(MSM.hasPart.getURI()).append("> ?part . }").append("\n");
-        queryBuilder.append(" UNION \n");
-        queryBuilder.append("{ ?message <").append(MSM.hasOptionalPart.getURI()).append("> ?part . }").append("\n");
-        queryBuilder.append(" UNION \n");
-        queryBuilder.append("{ ?message <").append(MSM.hasMandatoryPart.getURI()).append("> ?part . }").append("\n");
+        queryBuilder.append("{ ?message <").append(MSM.hasMandatoryPart.getURI()).append(">+ ?part . }").append("\n");
         queryBuilder.append(" ?part <").append(SAWSDL.modelReference.getURI()).append("> <").append(modelReference.toASCIIString()).append("> .").append("\n } } ");
 
         return this.graphStoreManager.listResourcesByQuery(queryBuilder.toString(), "entity");
